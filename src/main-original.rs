e6df5a07f64fda2a1f148d20bb6d1f755a532533
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-04-11 16:01:51 -0500

    changed to use quick_error

--- src/main-original.rs
+++ src/main-original.rs
@@ -1,30 +1,58 @@
-#[macro_use]
-extern crate quick_error;
-
 use serde::{Deserialize, Serialize};
+use std::error::Error;
+use std::fmt;
 use std::fs::read_to_string;
 
 // This is a custom error type.
 // It enables callers that receive this kind of error
 // to handle different error causes differently.
-// These must implement the Error trait which requires
-// implementing the Debug and Display traits.
-quick_error! {
-    #[derive(Debug)]
-    pub enum GetDogsError {
-        BadFile(e: std::io::Error) {
-            display("bad file {}", e)
-            from()
-        }
-        BadJson(e: serde_json::error::Error) {
-            display("bad json {}", e)
-            from()
-        }
-    }
+// These must implement the Error trait (below) which requires
+// implementing the Debug (derived) and Display (below) traits.
+#[derive(Debug)]
+pub enum GetDogsError {
+    BadFile(std::io::Error),
+    BadJson(serde_json::error::Error),
 }
 
 // Make the variants of this enum directly available.
 use GetDogsError::*;
+
+// All of the Error trait methods have default implementations, so
+// no body is required here, but we will implement the source method.
+impl Error for GetDogsError {
+    // Returns the wrapped error, if any.
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        match *self {
+            // The wrapped error type is implicitly cast to the trait object
+            // type &Error because it implements the Error trait.
+            BadFile(ref e) => Some(e),
+            BadJson(ref e) => Some(e),
+        }
+    }
+}
+
+impl std::fmt::Display for GetDogsError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            BadFile(ref e) => write!(f, "bad file: {}", e),
+            BadJson(ref e) => write!(f, "bad JSON: {}", e),
+        }
+    }
+}
+
+// The "From" trait converts values of one type to another.
+// Having the following implementations enables
+// using the ? operator in the get_dogs3 function below.
+impl From<std::io::Error> for GetDogsError {
+    fn from(other: std::io::Error) -> Self {
+        BadFile(other)
+    }
+}
+impl From<serde_json::error::Error> for GetDogsError {
+    fn from(other: serde_json::error::Error) -> Self {
+        BadJson(other)
+    }
+}
 
 // This struct can be deserialized from JSON and serialized to JSON.
 #[derive(Deserialize, Serialize, Debug)]
@@ -43,7 +71,7 @@ struct Dog {
 // serde_json::error::Error from failing to parse the JSON.
 // This approach is fine when callers only need to
 // know if an error occurred and print an error message.
-fn get_dogs1(file_path: &str) -> Result<Vec<Dog>, Box<dyn std::error::Error>> {
+fn get_dogs1(file_path: &str) -> Result<Vec<Dog>, Box<dyn Error>> {
     let json = read_to_string(file_path)?;
     let dogs: Vec<Dog> = serde_json::from_str(&json)?;
     Ok(dogs)
